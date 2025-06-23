b244142be0c7938bc81c546474bf207b639890b8
ImtiazAhmed01 <imtiazahm21@gmail.com>
2025-06-23 14:30:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router'
+import './App.css'
+import Navbar from './Component/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </>
+  )
+}
+
+export default App
